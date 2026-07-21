0830c09de4fc4ef61a5eb577b43a8b3082dfcdab
Sameer Shaik <sameershaik.webdev@gmail.com>
2026-07-21 11:53:18 +0530

    for error identify

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,15 +1,7 @@
-import SceneCanvas from "@/components/three/core/SceneCanvas";
-import Hero from "@/components/hero/Hero";
-import Navbar from "@/components/navbar/Navbar";
-import About from "@/components/about/AboutHero";
-
 export default function HomePage() {
   return (
     <main>
-      <SceneCanvas />
-      <Navbar />
-      <Hero />
-      
+      <h1>M3 Global Services</h1>
     </main>
   );
 }
