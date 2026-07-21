98d9c82a60f2ad5175139f27a059fed6704d893e
Sameer Shaik <sameershaik.webdev@gmail.com>
2026-07-21 11:57:06 +0530

    Refactor HomePage to include SceneCanvas, Navbar, and Hero

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,15 @@
+import SceneCanvas from "@/components/three/core/SceneCanvas";
+import Hero from "@/components/hero/Hero";
+import Navbar from "@/components/navbar/Navbar";
+import About from "@/components/about/AboutHero";
+
 export default function HomePage() {
   return (
     <main>
-      <h1>M3 Global Services</h1>
+      <SceneCanvas />
+      <Navbar />
+      <Hero />
+      
     </main>
   );
 }
