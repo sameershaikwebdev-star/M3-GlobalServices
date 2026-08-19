271cd0c3f7532ee60d6c9f540d694ae368769635
Sameer Shaik <23BQ1A12F3@vvit.net>
2026-08-19 10:56:09 +0530

    Update spacing and styling in ServicesSection component

--- frontend/components/services/ServicesSection.tsx
+++ frontend/components/services/ServicesSection.tsx
@@ -3,12 +3,12 @@ import { services } from "./servicesData";
 
 export default function ServicesSection() {
   return (
-    <section className="pb-24">
+    <section className="space-y-20 pb-24 lg:space-y-28">
       {services.map((service, index) => (
         <div key={service.id}>
           <ServiceCard service={service} />
           {index !== services.length - 1 && (
-            <div className="mx-auto my-14 h-px max-w-6xl bg-cyan-400/30 lg:my-18" />
+            <div className="mx-auto mt-20 h-px max-w-6xl bg-cyan-400/30 px-6 lg:mt-28" />
           )}
         </div>
       ))}
